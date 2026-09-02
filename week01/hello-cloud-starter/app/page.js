72b6124370a082e8061ecc3e83d54cf72dd75be7
huduby777 <hudubcoding@gmail.com>
2026-09-02 22:45:18 +0900

    ok

--- week01/hello-cloud-starter/app/page.js
+++ week01/hello-cloud-starter/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>Hello Cloud!</h1>
-      <p>Let's go!Let's go!Let's go!Let's go!</p>
+      <p>OK...I got It</p>
     </main>
   );
 }
